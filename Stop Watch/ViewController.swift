4191097b014fc0be628cb803dcd37eb1ad58fc93
Jonghyun Kim <jhkim@dit.ac.kr>
2019-04-01 22:46:55 +0900

    1st version

--- Stop Watch/ViewController.swift
+++ Stop Watch/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    var counter = 0
+    var myTimer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        timeLabel.text = String(counter)
     }
 
-
+    @IBAction func timeStart(_ sender: Any) {
+        
+        if !myTimer.isValid {
+            myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+        } else {
+            print("Play button은 작동하지 않음")
+        }
+        
+    }
+    
+    @IBAction func timeStop(_ sender: Any) {
+        myTimer.invalidate()
+        
+    }
+    
+    @IBAction func timePause(_ sender: Any) {
+        myTimer.invalidate()
+        counter = 0
+        timeLabel.text = String(counter)
+        
+    }
+    
+    @objc func updateTime() {
+        counter+=1
+        timeLabel.text = String(counter)
+    }
+    
+    
 }
 
